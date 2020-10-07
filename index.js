b558c1eff01c9c96475ad2d8e0251be38c9abfc0
joilson <joilson.bica@somospi.com.br>
2020-10-06 22:11:37 -0300

    feature: adiciona funcionalidade de alterar velocidade

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const $ = document.querySelector.bind(document);
 const speech = $('#speech');
 const speechButton = $('#speech-button');
+const rateRange = $('#rate');
+const rateText = $('#rateText');
 const utterance = new SpeechSynthesisUtterance();
+let rate = rateRange.value;
 
 speechButton.addEventListener('click', () => {
     let speechText = speech.value;
     utterance.text = speechText;
     utterance.lang = 'pt-BR';
-    utterance.rate = 5;
+    utterance.rate = rate;
     speechSynthesis.speak(utterance);
 });
+
+rateRange.addEventListener('change', () => {
+    rate = rateRange.value;
+    rateText.innerHTML = rate;
+});
+
+function init() {
+    rateText.innerHTML = rate;
+}
+
+init();
