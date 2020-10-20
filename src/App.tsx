fd6082a769b8e0f5bfe36558e1540c4e4b97485b
joilson <joilson_rg@hotmail.com>
2020-10-20 19:00:26 -0300

    chore: adiciona elementos do speech-engine no app

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,34 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        header
-      </header>
+      <div className="main">
+        <div className="options">
+          <div>
+            <label htmlFor="rate">Velocidade: </label>
+            <input id="rate" type="range" min="1" max="10" step="1" value="1" />
+            <span id="rate-text"></span>
+          </div>
+          <div>
+            <label htmlFor="pitch">Entonação: </label>
+            <input id="pitch" type="range" min="0" max="2" step="0.1" value="1" />
+            <span id="pitch-text"></span>
+          </div>
+          <div>
+            <label htmlFor="volume">Volume: </label>
+            <input id="volume" type="range" min="0" max="1" step="0.01" value="1" />
+            <span id="volume-text"></span>
+          </div>
+        </div>
+        <div>
+          <textarea name="speech" id="speech" cols={120} rows={40}></textarea>
+          <span id="speech-size"></span>
+        </div>
+        <div> 
+          <button id="speech-button">Speech</button>
+          <button id="pause-button">Pause</button>
+          <button id="resume-button">Resume</button>
+        </div>
+      </div>
     </div>
   );
 }
