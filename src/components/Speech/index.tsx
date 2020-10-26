9621fcf45b96ce10513c48a99d2138e89a7559ad
joilson <joilson_rg@hotmail.com>
2020-10-26 18:56:50 -0300

    feature: adiciona funcionalidade de alterar valores dos range

--- src/components/Speech/index.tsx
+++ src/components/Speech/index.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
+import { parse } from 'url';
 
 import { Container } from './styles'
 
 const Speech: React.FC = () => {
+
+    const [ volume, setVolume ] = useState('100');
+    const [ pitch, setPitch ] = useState('1');
+    const [ rate, setRate ] = useState('1');
+
+    const changeVolume = (value: string) => {
+        setVolume((parseFloat(value) * 100).toFixed(0));
+    }
+
+    const changePitch = (value: string) => {
+        setPitch(parseFloat(value).toFixed(1));
+    }
+
+    const changeRate = (value: string) => {
+        setRate(parseFloat(value).toFixed(0));
+    }
+
     return (
         <>
             <div className="App">
@@ -10,18 +28,18 @@ const Speech: React.FC = () => {
                 <div className="options">
                     <div>
                         <label htmlFor="rate">Velocidade: </label>
-                        <input id="rate" type="range" min="1" max="10" step="1" defaultValue="1" />
-                        <span id="rate-text"></span>
+                        <input id="rate" type="range" min="1" max="10" step="1" defaultValue="1" onChange={e => changeRate(e.target.value)} />
+                        <span id="rate-text">{rate}</span>
                     </div>
                     <div>
                         <label htmlFor="pitch">Entonação: </label>
-                        <input id="pitch" type="range" min="0" max="2" step="0.1" defaultValue="1" />
-                        <span id="pitch-text"></span>
+                        <input id="pitch" type="range" min="0" max="2" step="0.1" defaultValue="1" onChange={e => changePitch(e.target.value)} />
+                        <span id="pitch-text">{pitch}</span>
                     </div>
                     <div>
                         <label htmlFor="volume">Volume: </label>
-                        <input id="volume" type="range" min="0" max="1" step="0.01" defaultValue="1" />
-                        <span id="volume-text"></span>
+                        <input id="volume" type="range" min="0" max="1" step="0.01" defaultValue="1" onChange={e => changeVolume(e.target.value)} />
+                        <span id="volume-text">{volume}</span>
                     </div>
                 </div>
                 <div>
