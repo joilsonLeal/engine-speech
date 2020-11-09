2f3d17ea23be4f5a7c14c0bb16e4b8264ec47ade
joilson <joilson_rg@hotmail.com>
2020-11-09 11:34:25 -0300

    chore: remove pitch and volume

--- src/components/Speech/index.tsx
+++ src/components/Speech/index.tsx
@@ -5,18 +5,8 @@ import { Container } from './styles'
 
 const Speech: React.FC = () => {
     const utterance = new SpeechSynthesisUtterance();
-    const [ volume, setVolume ] = useState('100');
-    const [ pitch, setPitch ] = useState('1');
     const [ rate, setRate ] = useState('1');
     const [ text, setText ] = useState('');
-
-    const changeVolume = (value: string) => {
-        setVolume((parseFloat(value) * 100).toFixed(0));
-    }
-
-    const changePitch = (value: string) => {
-        setPitch(parseFloat(value).toFixed(1));
-    }
 
     const changeRate = (value: string) => {
         setRate(parseFloat(value).toFixed(0));
@@ -42,16 +32,6 @@ const Speech: React.FC = () => {
                         <label htmlFor="rate">Velocidade: </label>
                         <input id="rate" type="range" min="1" max="10" step="1" defaultValue="1" onChange={e => changeRate(e.target.value)} />
                         <span id="rate-text">{rate}</span>
-                    </div>
-                    <div>
-                        <label htmlFor="pitch">Entonação: </label>
-                        <input id="pitch" type="range" min="0" max="2" step="0.1" defaultValue="1" onChange={e => changePitch(e.target.value)} />
-                        <span id="pitch-text">{pitch}</span>
-                    </div>
-                    <div>
-                        <label htmlFor="volume">Volume: </label>
-                        <input id="volume" type="range" min="0" max="1" step="0.01" defaultValue="1" onChange={e => changeVolume(e.target.value)} />
-                        <span id="volume-text">{volume}</span>
                     </div>
                 </div>
                 <div>
